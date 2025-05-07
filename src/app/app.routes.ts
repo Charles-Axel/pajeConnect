e7bef8e242dd4901085fed38cb104cf88c2286d7
Charles Axel <axel_charles@ens.univ-artois.fr>
2025-05-07 23:31:21 +0200

    :sparkles: Route Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {AccueilComponent} from "./features/accueil/accueil.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AccueilComponent},
+];
