b2ed020291852d915b6d5ec422ce710f0af1175f
Charles Axel <axel_charles@ens.univ-artois.fr>
2025-05-07 23:28:46 +0200

    :sparkles: SideNav Component

---
+++ src/app/layout/sidenav/nav-data.ts
@@ -0,0 +1,5 @@
+export const navBarData: { routeLink: string; icon: string; label: string }[] = [
+    { routeLink: '/accueil', icon: 'home', label: 'Accueil' },
+    { routeLink: '/constructions', icon: 'construction', label: 'Nos réalisations' },
+    { routeLink: '/entreprise', icon: 'business_center', label: "L'entreprise" },
+];
---
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -0,0 +1,38 @@
+import {Component, Output, EventEmitter} from '@angular/core';
+import {navBarData} from "./nav-data";
+import {RouterLink} from "@angular/router";
+import {MatIcon} from "@angular/material/icon";
+import {NgClass} from "@angular/common";
+
+interface SideNavToggle {
+    screenWidth: number;
+    collapsed: boolean;
+}
+
+@Component({
+  selector: 'app-sidenav',
+  imports: [
+    RouterLink,
+    MatIcon,
+    NgClass
+  ],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.css'
+})
+export class SidenavComponent {
+
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = false;
+  screenWidth = 0;
+  navData = navBarData;
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
+}
