60bd47c49a6cbde41fdd4cedb6eb033dacdbbf75
Charles Axel <axel_charles@ens.univ-artois.fr>
2025-05-07 23:29:13 +0200

    :sparkles: Accueil Component

---
+++ src/app/features/accueil/accueil.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-accueil',
+  imports: [
+    MatIcon
+  ],
+  templateUrl: './accueil.component.html',
+  styleUrl: './accueil.component.css'
+})
+export class AccueilComponent  {
+
+}
