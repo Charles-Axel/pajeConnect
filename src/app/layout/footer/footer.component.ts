8601e6603579ce1624d4b56813566a3b73ec877b
Charles Axel <axel_charles@ens.univ-artois.fr>
2025-05-08 02:32:04 +0200

    :sparkles: Footer Upgrade

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-footer',
     imports: [
-        RouterLink
+        MatIcon
     ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
